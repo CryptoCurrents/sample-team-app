01c3beec9c69a919b76501b084883ee4450c892a
krewskatee <lchira99@gmail.com>
2017-10-29 10:33:12 -0500

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   get '/' => 'pages#index'
-<<<<<<< HEAD
   get '/me' => 'pages#show'
-=======
-  get '/pages' => 'pages#show'
->>>>>>> d67d0f143e2af155672c5ee1293ad1067c789fd5
 end
