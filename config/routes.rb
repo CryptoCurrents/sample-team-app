8fbb9b7ae6f8acd71006212ac8e9704010b55380
ProfBologna <sam.aporia@gmail.com>
2017-10-29 10:58:05 -0500

    fixed routes errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'pages#index'
-
-<<<<<<< HEAD
-=======
   get '/pencils' => 'pencils#index'
   get '/pages' => 'pages#show'
   get '/me' => 'pages#show'
 
-
->>>>>>> be99a20abfdf73650958ca6f9de13e6b3342ee3f
   get '/oranges' => 'oranges#index' 
 
   get '/gaut/' => 'gaut#index'
@@ -24,11 +19,8 @@ Rails.application.routes.draw do
   get '/pages' => 'pages#show'
 
 
-<<<<<<< HEAD
-
   get '/' => 'streets#index'
 
-=======
   get '/tests' => 'tests#index'
 
 
@@ -37,14 +29,6 @@ Rails.application.routes.draw do
 
   get '/' => 'streets#index'
 
-
->>>>>>> be99a20abfdf73650958ca6f9de13e6b3342ee3f
   get '/me' => 'pages#show'
 
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> be99a20abfdf73650958ca6f9de13e6b3342ee3f
 end
